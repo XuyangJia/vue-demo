3bb6f518231267302d1628ed71075f34b401fad2
Xuyang Jia <jiaxuyang@outlook.com>
2019-04-17 20:32:11 +0800

    start programe home page

---
+++ src/components/tabbar/HomeContainer.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <mt-swipe :auto="4000">
+            <mt-swipe-item>1</mt-swipe-item>
+            <mt-swipe-item>2</mt-swipe-item>
+            <mt-swipe-item>3</mt-swipe-item>
+        </mt-swipe>
+        <h2>Home</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {}
+    },
+    methods: {
+        getLunboTu() {
+            console.log(` ------------------------${123}---------------------------- `)
+            this.$http.get('http://www.liulongbin.top:3005/api/getlunbo').then(result => {
+                console.log(result)
+                
+            })
+        }
+    },
+    created() {
+        this.getLunboTu()
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.mint-swipe {
+    height: 200px;
+    .mint-swipe-item {
+        &:nth-child(1) {
+            background-color: red;
+        }
+    }
+}
+</style>
+
--- src/router.js
+++ src/router.js
@@ -1,10 +1,12 @@
 import VueRouter from 'vue-router'
-
+import HomeContainer from './components/tabbar/HomeContainer.vue'
 
 // 3. 创建路由对象
 var router = new VueRouter({
   routes: [
-  ]
+    {path: '/home', component: HomeContainer}
+  ],
+  linkActiveClass: 'mui-active'
 })
 
 // 把路由对象暴露出去
